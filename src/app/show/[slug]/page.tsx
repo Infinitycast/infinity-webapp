c0055930924bb370979614b770eed935eb0d0238
Damian Ryan <damian@damianryan.co.uk>
2026-03-23 16:34:27 +0000

    removes block on show page

--- src/app/show/[slug]/page.tsx
+++ src/app/show/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { getShowBySlug } from "@/app/actions/getShowBySlug";
 import ShowLayout from "@/components/layouts/ShowLayout";
 import { getCurrentUser } from "@/lib/auth";
-import { redirect } from "next/navigation";
 
 export default async function ShowPage({
   params,
@@ -10,10 +9,6 @@ export default async function ShowPage({
 }) {
   const { slug } = await params;
   const user = await getCurrentUser();
-
-  if (!user) {
-    redirect("/auth/signin");
-  }
 
   const show = await getShowBySlug(slug);
   return <ShowLayout show={show} user={user} />;
